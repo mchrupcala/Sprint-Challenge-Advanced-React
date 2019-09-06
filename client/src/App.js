2ebc5d01e570ed786e98cbe4409549b96e6dc4ab
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-09-06 12:14:56 -0400

    Added the dark mode feature and successfully toggling that with dark styles.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
-import PlayerList from './components/PlayerList'
+import PlayerList from './components/PlayerList';
+import useDarkMode from './components/useDarkMode';
 
-function App() {
+const App = () => {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
 
   return (
+    <>
     <div className="App">
       <h1>Player List!</h1>
+        <div className="nav-bar">
+          <div className="dark-mode-toggle">
+            on
+            <div 
+            onClick={toggleMode}
+            className={darkMode ? 'toggle toggled' : 'toggle'}>
+
+            </div>
+        </div>
+      </div>
+
       <PlayerList />
     </div>
+    </>
   );
 }
 
