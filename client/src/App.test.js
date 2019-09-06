e75721b019551e8fa32cb7e7936ebed3f00cf301
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-09-06 11:52:15 -0400

    Successfully running and passing two basic tests.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { getByText } from '@testing-library/dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />);
 });
+
+test('header appears on the screen', () => {
+  let { getByText} = render(<App />);
+  
+  getByText(/player list/i);
+});
+
+// test('all four buttons are displaying on-screen', () => {
+//   let { getByText, getByTestId} = render(<App />);
+  
+//   getByTestId(/ball-button/i);
+//   getByTestId(/strike-button/i);
+
+// });
